e81e427493e9955668b58ebc007819e43151c7c8
Kiran Anand <liteninkiran@hotmail.com>
2022-08-13 15:11:49 +0100

    Fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            CreateAdminUserSeeder::class,
             PermissionTableSeeder::class,
+            CreateAdminUserSeeder::class,
         ]);
     }
 }
